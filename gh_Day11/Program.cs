95cd150ba4c9f05b99d24c26e2b3b1837865b738
jiyeonUning <yeon14735@gmail.com>
2024-08-01 13:36:13 +0900

    과제. 몬스터 데이터베이스구현하기
    Main 출력을 제외한, 과제에서 구현을 요구한 클래스들의 정보를 구현하였습니다.

---
+++ gh_Day11/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace gh_Day11
+{
+    /* 과제 1. 몬스터 데이터베이스 구현하기
+     
+               다음의 조건을 충족하는 프로그램을 구현하시오.
+
+      구현 클래스 :
+                    MonsterData
+                    Monster
+
+               프로그램 시작 시, MonsterData는 몬스터 이름 기준의 string Key 값으로 딕셔너리에 저장한다. (최소 5종류 이상)
+               Monster 클래스의 인스턴스 생성 시, 생성자를 사용해 딕셔너리에 저장된 MonsterData 클래스의 정보를 불러와 인스턴스의 데이터를 초기화해야한다.
+    */
+
+
+    public class Program
+    {
+        static void Main()
+        {
+            // 메인에 딕셔너리에 저장된 정보를 출력하는 방법을 모르겠음
+        }
+    }
+
+
+    public enum MonsterType { Fire, Water, Electric, Nomal, Fairy }
+
+    public class MonsterData // MonsterData 클래스 생성
+    {
+        public MonsterData(Monster monster)
+        {
+            // 프로그램 시작시, MonsterData는 몬스터 이름 기준의 string Key 값으로 딕셔너리에 저장 (5종류 이상)
+            Dictionary<string, Monster> monsterETC = new Dictionary<string, Monster>();
+            monsterETC.Add("파이리", new Monster("파이리", MonsterType.Fire, 80));
+            monsterETC.Add("울머기", new Monster("울머기", MonsterType.Water, 120));
+            monsterETC.Add("데덴네", new Monster("데덴네", MonsterType.Electric, 30));
+            monsterETC.Add("이브이", new Monster("이브이", MonsterType.Nomal, 50));
+            monsterETC.Add("님피아", new Monster("님피아", MonsterType.Fairy, 200));
+        }
+    }
+
+
+    public class Monster // Monster 클래스 생성
+    {
+        public string name;
+        public MonsterType type;
+        public int hp;
+
+        // Monster 클래스의 인스턴스 생성 시, 생성자를 사용해
+        // 딕셔너리에 저장된 MonsterData 클래스의 정보를 불러와 해당 인스턴스의 데이터를 초기화
+        public Monster(string name, MonsterType type, int hp)
+        {
+            name = "몬스터 이름";
+            type = MonsterType.Nomal;
+            hp = 100;
+        }
+    }
+
+
+}
